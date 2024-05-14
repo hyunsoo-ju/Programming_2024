64b8e133f3dbc4695f949f79fcee192121b72d56
hyunsoo-ju <hyunsoo-ju@gm.gist.ac.kr>
2024-05-14 20:48:30 +0900

    changed compiler to gcc

---
+++ test.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+    int a = 10;
+    int b = 20;
+    int c;
+    
+    printf("Hello World!\n");
+    printf("Hello World!\n");
+    printf("Hello World!\n");
+    printf("Hello World!\n");
+    printf("Hello World!\n");
+    printf("Hello World!\n");
+
+    c = a + b;
+
+    return 0;
+}
